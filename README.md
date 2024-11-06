0d109534c594a1400f09697419fcddac0d48f6a0
psychosis448 <psychosis448@protonmail.com>
2024-11-06 15:50:21 +0100

    Init lazyvim-nix template

---
+++ README.md
@@ -0,0 +1,46 @@
+#  Nix + 💤 LazyVim
+
+A minimal starter template for [LazyVim](https://github.com/LazyVim/LazyVim)
+on [Nix](https://nixos.org/)
+with [Home Manager](https://nix-community.github.io/home-manager/)
+using [Lua](https://www.lua.org/),
+based on the [lazy.nvim on NixOS](https://nixalted.com/lazynvim-nixos.html)
+guide by [KFerasoff](https://github.com/KFearsoff).
+
+The template uses Nix to manage Neovim as a software packages and its plugins.
+But it uses Lua for configuration of Neovim and the plugins themselves.
+All the LazyVim plugins are disabled, so that you can start from a (somewhat) clean sheet
+and decide for yourself which plugins you want to enable.
+Nevertheless, all the LazyVim default [autocmds](./lua/config/autocmds.lua), [keymaps](./lua/config/keymaps.lua)
+and [options](./lua/config/options.lua) are still enabled, which might easily lead to errors.
+So you might want to disable unused autocmds/keymaps/options that rely on not installed plugins!
+
+Please check KFerasoff's guide for more details about the general setup.
+
+## Installing plugins
+
+As mentioned, we manage the installation of Neovim and its plugins with Nix.
+Let's add and configure a custom colorscheme, e.g. [Modus Themes](https://github.com/miikanissi/modus-themes.nvim).
+
+1. Add plugin to `programs.neovim.plugins` in `default.nix` (e.g. `modus-themes-nvim`)
+2. Configure plugin in `./lua/plugins` directory (e.g. `lazyvim.lua`)
+3. Rebuild your configuration
+
+## Enabling LazyVim Plugins
+
+Same procedure as mentioned above:
+
+1. Add plugin to `programs.neovim.plugins` in `default.nix` (e.g. `which-key`)
+2. Optional: configure plugin in `./lua/plugins` directory (e.g. `which-key.lua`)
+3. Rebuild your configuration
+
+## Enabling LazyVim Extras
+
+To enable [LazyVim Extras](https://www.lazyvim.org/extras), you simply follow the above pattern with a small change:
+
+1. Add plugin to `programs.neovim.plugins` in `default.nix` (e.g. `mini-surround`)
+2. Import plugin configuration in `default.nix`, in the lazy setup section of the `extraLuaConfig` part:
+    `{ import = "lazyvim.plugins.extras.coding.mini-surround" }`
+3. Optional: configure plugin in `./lua/plugins` directory (e.g. `mini-surround.lua`)
+4. Rebuild your configuration
+
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,89 @@
+-- NOTE: disabled all LazyVim plugins
+return {
+  -- CODING
+  -- nvim-cmp
+  { "hrsh7th/nvim-cmp",                            enabled = false },
+  { "hrsh7th/cmp-nvim-lsp",                        enabled = false },
+  { "hrsh7th/cmp-nvim-buffer",                     enabled = false },
+  { "hrsh7th/cmp-nvim-path",                       enabled = false },
+  -- nvim-snippets
+  { "garymjr/nvim-snippets",                       enabled = false },
+  { "rafamadriz/friendly-snippets",                enabled = false },
+  -- mini.pairs
+  { "echasnovski/mini.pairs",                      enabled = false },
+  -- ts-comments.nvim
+  { "folke/ts-comments.nvim",                      enabled = false },
+  -- mini.ai
+  { "mini.ai",                                     enabled = false },
+  -- lazydev.nvim
+  { "folke/lazydev.nvim",                          enabled = false },
+  { "Bilal2453/luvit-meta",                        enabled = false },
+
+  -- COLORSCHEME
+  { "folke/tokyonight.nvim",                       enabled = false },
+  { "catppuccin/nvim",                             enabled = false },
+
+  -- EDITOR
+  -- neo-tree.nvim
+  { "nvim-neo-tree/neo-tree.nvim",                 enabled = false },
+  -- grug-far.nvim
+  { "MagicDuck/grug-far.nvim",                     enabled = false },
+  -- flash.nvim
+  { "folke/flash.nvim",                            enabled = false },
+  -- which-key.nvim
+  { "folke/which-key.nvim",                        enabled = false },
+  -- gitsigns.nvim
+  { "lewis6991/gitsigns.nvim",                     enabled = false },
+  -- trouble.nvim
+  { "folke/trouble.nvim",                          enabled = false },
+  -- todo-comments.nvim
+  { "folke/todo-comments.nvim",                    enabled = false },
+
+  -- FORMATTING
+  -- conform.nvim
+  { "stevearc/conform.nvim",                       enabled = false },
+  -- mason.nvim
+  -- WARNING: don't enable on NixOS
+  { "mason.nvim",                                  enabled = false },
+
+  -- LINTING
+  -- nvim-lint
+  { "mfussenegger/nvim-lint",                      enabled = false },
+
+  -- LSP
+  -- nvim-lspconfig
+  { "neovim/nvim-lspconfig",                       enabled = false },
+  -- WARNING: don't enable on NixOS
+  { "williamboman/mason-lspconfig.nvim",           enabled = false },
+
+  -- TREESITTER
+  -- nvim-treesitter
+  { "nvim-treesitter/nvim-treesitter",             enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  -- nvim-ts-autotag
+  { "windwp/nvim-ts-autotag",                      enabled = false },
+
+  -- UI
+  -- nvim-notify
+  { "rcarriga/nvim-notify",                        enabled = false },
+  -- bufferline.nvim
+  { "akinsho/bufferline.nvim",                     enabled = false },
+  -- lualine.nvim
+  { "nvim-lualine/lualine.nvim",                   enabled = false },
+  -- indent-blankline.nvim
+  { "lukas-reineke/indent-blankline.nvim",         enabled = false },
+  -- noice.nvim
+  { "folke/noice.nvim",                            enabled = false },
+  -- mini.icons
+  { "echasnovski/mini.icons",                      enabled = false },
+  -- nui.nvim
+  { "MunifTanjim/nui.nvim",                        enabled = false },
+  --dashboard-nvim
+  { "nvimdev/dashboard-nvim",                      enabled = false },
+
+  -- UTIL
+  -- persistence.nvim
+  { "folke/persistence.nvim",                      enabled = false },
+  -- plenary.nvim
+  { "nvim-lua/plenary.nvim",                       enabled = false },
+}
